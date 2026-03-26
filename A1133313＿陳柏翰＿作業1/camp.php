617946760c545473f137ea244aa7b7a4c6ee4632
a1133313-bohan <a1133313@mail.nuk.edu.tw>
2026-03-27 00:54:45 +0800

    Add files via upload

---
+++ A1133313＿陳柏翰＿作業1/camp.php
@@ -0,0 +1,98 @@
+<html>
+
+<head>
+
+<title>夏令營報名表!!</title>
+
+
+</head>
+
+<body>
+
+<body style="background-color: lightgray;">
+
+<br>
+<center><font size="10" color="red">!!!!夏資一定!!!!</font></center>
+<center><font size="6" color="deeppink">²⁰²⁵高大資管夏令營《資想和你浪一下》</font></center>
+
+
+<br>
+    <br><font size="4">
+還沒想好夏天要去哪裡玩？<br>
+答案就是跟我們浪去墾丁一起抓住夏天的尾巴吧！</font>
+<br>
+    <br>
+
+<center><img src="./one.png" width="30%"><br>
+            <a href ="https://www.instagram.com/nukim_december.118/" target ="blank">想了解更多點這邊</a>
+</center>
+<br>
+
+<table border="0" width="100%">
+
+    <td width="50%" valign="top">
+        <li><font size="5" color="green">集合地點：管院門口
+            <li>集合時間---08:30 ~ 08:50
+                <li>09:00 準時出發，請務必準時</font></li>
+    </td>
+
+    <td width="50%" valign="top">
+      <li><font size="5" color="green">記得多帶一套換洗衣物
+        <li>活動期間有課的話記得請假!!
+            <li>平平安安出門 快快樂樂回家</font></li>
+    </td>
+
+<br>
+<table border="15" width="40%" height="27%">
+<tr><th></th><th bgcolor="#69e9d2">DAY1</th><th bgcolor="#0bb13d">DAY2</th><th bgcolor="#3573ef">DAY3</th></tr>
+<tr><td>早上</td><td>抵達墾丁</td><td>闖關活動</td><td>早餐吃到飽</td></tr>
+<tr><td>下午</td><td>料理大賽</td><td>沙灘戲水</td><td>賭場活動</td></tr>
+<tr><td>晚上</td><td>酒精對決</td><td>卡拉ok</td><td>商圈購物</td></tr>
+<br>
+</table>
+
+<br>
+<center>
+    <font size="6" color="deeporange">-------------- 來報名吧 --------------</font>
+</center>
+
+<font size="4">
+<form action="" method="">
+    
+<br>
+你的名字:<input type="text" palceholder="Your name" name="nName"><br>
+電話:<input type="text" palceholder="Your phone number" name="nNum"><br>
+
+E-mail
+<input type="email" name="mEmail" value="@"><br>
+
+Favor:
+葷<input type="radio" name="mDeal" value="m">
+素<input type="radio" name="mDeal" value="v">
+<br>
+
+來自哪裡?
+<select name="nCity">
+<option value="Kaohsiung">高雄</option>
+<option value="Taichung">台中</option>
+<option value="Taipei">台北</option>
+<option value="Tainan">台南</option>
+<option value="Hsinchu">新竹</option>
+<option value="Taoyuan">桃園</option>
+<option value="Hualien">花蓮</option>
+<option value="New Taipei">新北</option>
+<option value="Chiayi">嘉義</option>
+</select>
+<br>
+
+<textarea name="comment" rows="10" cols="50">
+leave somethings~~
+
+</textarea><br>
+<input type="submit">&nbsp&nbsp<input type="reset">
+
+</font>
+
+</body>
+
+</html>
